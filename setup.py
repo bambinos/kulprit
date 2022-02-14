2673b18a27df2b1c4586e24f008ce07e794aef83
Yann McLatchie <yann@mclatchie.com>
2022-02-14 16:30:07 +0000

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="pyprojpred",
+    packages=find_packages(),
+    version="0.1.0",
+    description="projection predictive variable selection",
+    author="Yann McLatchie",
+    license="MIT",
+)
