fe88850c48e87f9881d0b8d95553f59a4e7e9c50
Yann McLatchie <yann@mclatchie.com>
2022-04-14 18:42:40 +0300

    update tests for new functionality

--- tests/test_model_data.py
+++ tests/test_model_data.py
@@ -3,7 +3,6 @@ import numpy as np
 
 import bambi as bmb
 import kulprit as kpt
-from kulprit.data.building import _build_restricted_model
 
 import pytest
 
@@ -40,7 +39,7 @@ def test_no_intercept_error():
 
 
 def test_default_reference_model():
-    res_model = _build_restricted_model(proj.ref_model)
+    res_model = proj._build_restricted_model()
     assert res_model.X.shape == (
         proj.ref_model.num_obs,
         proj.ref_model.num_terms,
@@ -49,16 +48,16 @@ def test_default_reference_model():
 
 def test_build_restricted_model():
     model_size = 2
-    res_model = _build_restricted_model(proj.ref_model, model_size)
+    res_model = proj._build_restricted_model(model_size=model_size)
     assert res_model.X.shape == (proj.ref_model.num_obs, model_size + 1)
     assert res_model.model_size == model_size
 
 
 def test_build_negative_size_restricted_model():
     with pytest.raises(UserWarning):
-        _build_restricted_model(proj.ref_model, model_size=-1)
+        proj._build_restricted_model(model_size=-1)
 
 
 def test_build_too_large_restricted_model():
     with pytest.raises(UserWarning):
-        _build_restricted_model(proj.ref_model, model_size=100)
+        proj._build_restricted_model(model_size=100)
