8d7c0cae5d7aeae4036ce425ab49493153306763
aloctavodia <aloctavodia@gmail.com>
2024-12-27 18:33:25 -0300

    add l1

--- tests/test_search.py
+++ tests/test_search.py
@@ -1,5 +1,9 @@
 import copy
 import pytest
+import bambi as bmb
+import numpy as np
+from kulprit.projection.search_strategies import _first_non_zero_idx
+from kulprit import ProjectionPredictive
 
 
 class TestSearch:
@@ -14,35 +18,32 @@ class TestSearch:
         ref_model_copy.project()
         assert [submodel.size for submodel in ref_model_copy.submodels([0, 1, 2])] == [0, 1, 2]
 
-    # def test_l1(self, ref_model):
-    #     """Test that L1 search gives expected result."""
+    def test_l1(self, ref_model):
+        """Test that L1 search gives expected result."""
 
-    #     ref_model_copy = copy.copy(ref_model)
-    #     ref_model_copy.search(path="l1")
-    #     assert [submodel.size for submodel in ref_model_copy.submodels] == [0, 1, 2]
+        ref_model_copy = copy.copy(ref_model)
+        ref_model_copy.project(path="l1")
+        assert [submodel.size for submodel in ref_model_copy.submodels([0, 1, 2])] == [0, 1, 2]
 
-    # def test_l1_utils(self, ref_model):
-    #     """Test that L1 utility methods return expected result."""
+    def test_l1_utils(self):
+        """Test that L1 utility methods return expected result."""
+        arr = np.array([[0.0, 1.0, 2.0, 3.0], [0.0, 0.0, -1.0, 2.0], [0.0, 0.0, 0.0, 0.0]])
+        assert _first_non_zero_idx(arr) == {0: 1, 1: 2, 2: np.inf}
 
-    #     proj = ref_model.projector
-    #     searcher = L1SearchPath(proj)
-    #     arr = np.array([[0.0, 1.0, 2.0, 3.0], [0.0, 0.0, -1.0, 2.0], [0.0, 0.0, 0.0, 0.0]])
-    #     assert searcher.first_non_zero_idx(arr) == {0: 1, 1: 2, 2: np.inf}
+    def test_l1_categorical_error(self):
+        """Test that an error is raised when no search path is found."""
 
-    # def test_l1_categorical_error(self):
-    #     """Test that an error is raised when no search path is found."""
+        data = bmb.load_data("carclaims")[::50]
+        model_cat = bmb.Model("claimcst0 ~ C(agecat) + gender + area", data, family="gaussian")
+        fitted_cat = model_cat.fit(
+            draws=100,
+            tune=100,
+            idata_kwargs={"log_likelihood": True},
+        )
 
-    #     data = bmb.load_data("carclaims")[::50]
-    #     model_cat = bmb.Model("claimcst0 ~ C(agecat) + gender + area", data, family="gaussian")
-    #     fitted_cat = model_cat.fit(
-    #         draws=100,
-    #         tune=100,
-    #         idata_kwargs={"log_likelihood": True},
-    #     )
-
-    #     with pytest.raises(NotImplementedError):
-    #         ref_model = kpt.ProjectionPredictive(model=model_cat, idata=fitted_cat)
-    #         ref_model.search(method="l1")
+        with pytest.raises(NotImplementedError):
+            ref_model = ProjectionPredictive(model=model_cat, idata=fitted_cat)
+            ref_model.project(path="l1")
 
     def test_bad_search_method(self, ref_model):
         """Test that an error is raised when an invalid search method is used."""
