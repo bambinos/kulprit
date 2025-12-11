c5caf0066efdd4c50c8b51fb22b271dae12e7577
aloctavodia <aloctavodia@gmail.com>
2025-12-11 14:59:08 +0200

    bump release

--- kulprit/__init__.py
+++ kulprit/__init__.py
@@ -7,7 +7,7 @@ Kullback-Leibler projections for Bayesian model selection.
 from kulprit.projector import ProjectionPredictive
 from kulprit.plots import plot_compare, plot_dist, plot_forest
 
-__version__ = "0.5.0"
+__version__ = "0.5.1"
 
 
 __all__ = ["ProjectionPredictive", "plot_compare", "plot_dist", "plot_forest"]
