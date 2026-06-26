e0f541a985859bb3f77ee9a0c062d642c6f3f5d0
Osvaldo A Martin <aloctavodia@gmail.com>
2026-06-26 10:46:25 +0300

    Update __init__.py

--- kulprit/__init__.py
+++ kulprit/__init__.py
@@ -7,7 +7,7 @@ Kullback-Leibler projections for Bayesian model selection.
 from kulprit.projector import ProjectionPredictive
 from kulprit.plots import plot_compare, plot_dist, plot_forest
 
-__version__ = "0.6.0"
+__version__ = "0.6.1"
 
 
 __all__ = ["ProjectionPredictive", "plot_compare", "plot_dist", "plot_forest"]
